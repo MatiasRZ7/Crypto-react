a83abce7cfcb4171711eee8588f9128000ce3754
MatiasRZ7 <matiaz777@hotmail.com>
2024-02-24 12:40:42 +0100

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+import Coin from './components/Coin';
 function App() {
+  
+  const [listOfCoins, setListOfCoins] = useState([]);
+  const [searchWord, setSearchWord] = useState('');
+  useEffect(() => {
+    const options = {
+      headers: {
+        accept: 'application/json',
+        'X-API-KEY': 'JChwIcFCV5Ac2OsHbZh7chZlcBmfI1L4078BIWg62jQ='
+      }
+    };
+
+    Axios.get('https://openapiv1.coinstats.app/coins?', options)
+      .then(response => {
+        setListOfCoins(response.data.result);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
+
+  const filteredCoins = listOfCoins.filter((coin)=> {
+    return coin.name.toLowerCase().includes(searchWord.toLowerCase())
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='cryptoHeader'>
+          <input type='text' placeholder='Bitcoin...'
+          onChange={(e) => setSearchWord(e.target.value)}/>
+        </div>
+        <div className='cryptoDisplay'>
+        {filteredCoins.map((coin) => {
+      return <Coin key={coin.id} name={coin.name} icon={coin.icon} price={coin.price.toFixed(4)} symbol={coin.symbol} />
+      })}
+        </div>
     </div>
   );
 }
